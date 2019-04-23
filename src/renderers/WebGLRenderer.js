3a88f4b2b8f8bcf7d279db9b2ff605e69932c37e
Fernando Serrano <fernandojsg@gmail.com>
2019-04-23 10:35:45 +0200

    Remove multiviewEnabled on renderer

--- src/renderers/WebGLRenderer.js
+++ src/renderers/WebGLRenderer.js
@@ -316,7 +316,6 @@ function WebGLRenderer( parameters ) {
 	this.vr = vr;
 
 	var multiview = this.multiview = new WebGLMultiview(_multiviewRequested, _gl, _canvas, extensions, capabilities );
-	var multiviewEnabled = this.multiviewEnabled = multiview.isEnabled();
 
 	// shadow map
 
@@ -1364,7 +1363,7 @@ function WebGLRenderer( parameters ) {
 
 	function renderObjects( renderList, scene, camera, overrideMaterial ) {
 
-		if ( multiviewEnabled ) {
+		if ( multiview.isEnabled() ) {
 
 			multiview.bindMultiviewFrameBuffer( camera );
 
@@ -1758,7 +1757,7 @@ function WebGLRenderer( parameters ) {
 
 		if ( refreshProgram || _currentCamera !== camera ) {
 
-			if ( multiviewEnabled ) {
+			if ( multiview.isEnabled() ) {
 
 				if ( false && vr.isPresenting() ) {
 
@@ -1823,7 +1822,7 @@ function WebGLRenderer( parameters ) {
 				material.isShaderMaterial ||
 				material.skinning ) {
 
-				if ( multiviewEnabled ) {
+				if ( multiview.isEnabled() ) {
 
 					if ( vr.isPresenting() ) {
 
